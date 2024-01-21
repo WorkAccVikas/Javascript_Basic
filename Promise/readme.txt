098aba5d6386380931fea909767892fc2477a554
WorkAccVikas <workaccvikas@gmail.com>
2024-01-21 21:36:13 +0530

    c9

---
+++ Promise/readme.txt
@@ -0,0 +1,5 @@
+Definition : 
+    - The Promise object represents the eventual completion (or failure) of an asynchronous operation and its resulting value.
+
+We attach function means either it go to then if resolve or catch if reject 
+Execute only once
--- Promise/six.js
+++ Promise/six.js
@@ -37,6 +37,11 @@ let p5 = new Promise((resolve, reject) => {
   }, 1000);
 });
 
+/** DESC : all :
+ *  - When any one reject then catch block
+ *  - When all resolved then then block
+ */
+// * : 1/ Promise.all
 // let promise_all = Promise.all([p5, p2, p3]);  // ! : in catch block : p5 rejected
 // let promise_all = Promise.all([p1, p2, p3]);  // * : in then block : all promises resolved
 
@@ -53,6 +58,9 @@ let p5 = new Promise((resolve, reject) => {
 //     console.log("promise_all : Error in catch");
 //   });
 
+/** DESC : allSettled :
+ *  - Always go to then block
+ */
 // *: 2. Promise.allSettled
 // let promise_all_settled = Promise.allSettled([p1, p2, p3]); // * all promises resolved : execute then block
 // let promise_all_settled = Promise.allSettled([p1, p2, p4]); // * : here p4 is rejected => execute then block
@@ -75,9 +83,18 @@ let p5 = new Promise((resolve, reject) => {
 //     console.log("promise_all_settled : Error in catch : ", err);
 //   });
 
+/** DESC : race :
+ *  - order doesn't matter
+ *  - If error takes 1s and fastest take 2s then catch block (e.g. 1)
+ *  - When all resolved then fastest value return (e.g. 2)
+ *  - If fastest resolved take 1s and error also take 1s then return resolved value (means then block) (e.g. 3)
+ *  - If all rejected then catch block (e.g. 4)
+ */
 // *: 3. Promise.race : order doesn't matter, if fastest promise failed then catch else return value
 // let promise_race = Promise.race([p5, p2, p3]);    // ! : in catch because p5 is rejected
 // let promise_race = Promise.race([p3, p2, p1]); // * : in then because all promises resolved ,return single value (fastest result) ,here p1
+// let promise_race = Promise.race([p1, p5, p3]);
+// let promise_race = Promise.race([p5, p4]);
 
 // console.log("Promise.race");
 
@@ -89,10 +106,17 @@ let p5 = new Promise((resolve, reject) => {
 //     console.log("promise_race : Error in catch");
 //   });
 
+/** DESC : any:
+ *  - order doesn't matter
+ *  - When all resolved then fastest value return (e.g. 1)
+ *  - When fastest was error it will return value if other promise are resolved if resolved then return that fastest value (e.g. 2) 
+ *    - Here, p5 reject : 1s , p2 resolve : 2s => return p2 result value
+ *  - If all rejected then catch block (e.g. 3)
+ */
 // *: 4. Promise.any : order doesn't matter, if fast1 failed it check fast 2 and so on... . If all fails then show AggregateError else success
 // let promise_any = Promise.any([p1, p2, p3]); // * : here p1 value is resolved first => in then ,return single value (fastest result) ,here p1
 // let promise_any = Promise.any([p5, p2, p3]); // * : here p5 is rejected => p2 value is resolved because first resolved
-// let promise_any = Promise.any([p5, p4]); // ! : here p4,p5 are rejected => execute catch block
+// let promise_any = Promise.any([p5, p4]); // ! : here p4,p5 are rejected => execute catch block (AggregateError)
 
 // console.log("Promise.any");
 
